7f2f1773e9cc83fc843e89c7e624204567e63b4a
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-06-21 14:00:18 +0200

    cleaned env vars for google storage

--- client/src/appConfig.js
+++ client/src/appConfig.js
@@ -1,6 +1,6 @@
-const CONFIG = {
-    HTTP_URL: 'http://localhost:5000',
-    WS_URL: 'ws://localhost:5000/subscriptions',
-}
+/* HTTP_URL: 'http://localhost:5000',
+    WS_URL: 'ws://localhost:5000/subscriptions', */
+const HTTP_URL = 'https://fakebook-fake-facebook.herokuapp.com/'
+const WS_URL = 'ws://fakebook-fake-facebook.herokuapp.com/subscriptions'
 
-export default CONFIG
+export { HTTP_URL, WS_URL }
--- server/services/firebaseStorage.js
+++ server/services/firebaseStorage.js
@@ -2,7 +2,19 @@ const admin = require('firebase-admin')
 const sharp = require('sharp')
 const generateRandomString = require('../utils/generateRandomString')
 
-let serviceAccount = require('./config.json')
+let serviceAccount = {
+    type: 'service_account',
+    project_id: 'fake-facebook-2a769',
+    private_key_id: process.env.GOOGLE_STORAGE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_STORAGE_PRIVATE_KEY,
+    client_email: 'firebase-adminsdk-lxjff@fake-facebook-2a769.iam.gserviceaccount.com',
+    client_id: '118171924157745807886',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+        'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-lxjff%40fake-facebook-2a769.iam.gserviceaccount.com',
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
