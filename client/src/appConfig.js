89c304457ca513651b366e91115f6df9dc67f070
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-06-21 21:37:49 +0200

    cleaning up constants

--- client/src/appConfig.js
+++ client/src/appConfig.js
@@ -1,6 +1,7 @@
 /* HTTP_URL: 'http://localhost:5000',
     WS_URL: 'ws://localhost:5000/subscriptions', */
-const HTTP_URL = 'https://fakebook-fake-facebook.herokuapp.com/'
-const WS_URL = 'ws://fakebook-fake-facebook.herokuapp.com/subscriptions'
+export const HTTP_URL = 'https://fakebook-fake-facebook.herokuapp.com/'
+export const WS_URL = 'ws://fakebook-fake-facebook.herokuapp.com/subscriptions'
 
-export { HTTP_URL, WS_URL }
+export const GOOGLE_AUTH_CLIENT_ID =
+    '160241737973-8rqa8gnbavqb6spmtgilv9rkjfnfbd9c.apps.googleusercontent.com'
